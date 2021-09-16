65a883688af83972ad4ab94c52ca96bd109529ce
Sean Terry <seanterry@Seans-MacBook-Pro.local>
2021-09-16 16:09:39 -0700

    Tasks 5-6 completed

--- index.js
+++ index.js
@@ -182,10 +182,17 @@ class Student extends Lambdasian {
    }
 
    listSubjects () {
-     return `Loving ${this.favSubjects.join(', ')}!`
+     return `Loving ${this.favSubjects.join(', ')}!`;
    }
 
-   
+   PRAssignment (subject) {
+     return `${this.name} has submitted a PR for ${subject}`;
+   }
+
+   sprintChallenge (subject) {
+     return `${this.name} has begun sprint challenge on ${subject}`;
+   }
+
 }
 
 /*
@@ -201,8 +208,21 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-   
+class ProjectManager extends Instructor {
+   constructor (pm) {
+     super(pm);
+     this.gradClassName = pm.gradClassName;
+     this.favInstructor = pm.favInstructor;
+   }
+
+   standUp (channel){
+     return `${this.name} announces to ${channel}, @${channel} standy times!`;
+   }
+
+   debugsCode (student, subject) {
+     return `${this.name} debugs ${student.name}'s code on ${subject}`;
+   }
+
 }
 /*
   STRETCH PROBLEM (no tests!)
